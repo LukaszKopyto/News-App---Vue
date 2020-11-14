3107d65804f6e9bd2ce2b20058dc2d044f48e50f
Lucas <lukasz.kopyto@gmail.com>
2020-11-14 16:07:02 +0100

    Fetch first 10 newest articles

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -38,23 +34,27 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import ApiClient from './services/ApiService';
 
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
   data: () => ({
-    //
+    data: {},
   }),
+  created() {
+    try {
+      ApiClient.getArticles().then((res) => {
+        this.data = res.data.response;
+      });
+    } catch (err) {
+      // eslint-disable-next-line
+      console.log(err);
+    }
+  },
 };
 </script>
---
+++ src/services/ApiService.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: 'https://content.guardianapis.com',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+});
+
+const params = new URLSearchParams([
+  ['api-key', process.env.VUE_APP_API_KEY],
+]);
+
+export default {
+  getArticles() {
+    return apiClient.get('/search', { params });
+  },
+};
