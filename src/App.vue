f9ce71a7c133696a30667782b253f89f0271a899
Lucas <lukasz.kopyto@gmail.com>
2020-11-15 13:04:50 +0100

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -32,6 +32,25 @@
         </v-row>
       </v-container>
     </v-main>
+    <v-footer
+      color="blue darken-4"
+      dark
+    >
+      <v-row
+        justify="center"
+        no-gutters
+      >
+        <v-col
+          cols="12"
+          class="d-flex flex-column justify-center align-center"
+        >
+          <strong class="mt-4">News App {{ new Date().getFullYear() }}</strong>
+          <p class="caption text-uppercase">
+            Keep up with breaking news
+          </p>
+        </v-col>
+      </v-row>
+    </v-footer>
   </v-app>
 </template>
 
