84cae95f5780482d49aa95872f3a7029c09c6508
Lucas <lukasz.kopyto@gmail.com>
2020-11-15 18:19:05 +0100

    Add hamburger menu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,28 +1,48 @@
 <template>
-  <v-app-bar
-    app
-    color="blue darken-4"
-    dark
-    flat
-    extended
-    class="d-flex align-center"
-  >
-    <div class="d-flex flex-column">
-      <h1
-        class="font-weight-black text-uppercase amber--text"
-        :style="$vuetify.breakpoint.xs && {'font-size': '1.5rem'}"
+  <nav>
+    <v-app-bar
+      app
+      color="blue darken-4"
+      dark
+      flat
+      extended
+      class="py-5"
+    >
+      <div
+        class="d-flex flex-column"
       >
-        News App
-      </h1>
-      <span class="subheading">Keep up with breaking news</span>
-    </div>
+        <h1
+          class="font-weight-black text-uppercase amber--text"
+        >
+          News <span class="font-weight-light">App</span>
+        </h1>
+        <span class="subheading">Keep up with breaking news</span>
+      </div>
 
-    <v-spacer />
-  </v-app-bar>
+      <v-spacer />
+
+      <v-app-bar-nav-icon
+        class="amber--text"
+        @click="drawer = !drawer"
+      />
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      class="blue darken-4"
+    >
+      <p>test</p>
+    </v-navigation-drawer>
+  </nav>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      drawer: false,
+    };
+  },
 };
 </script>
